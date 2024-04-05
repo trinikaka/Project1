eeb6a793abd7e07909203397f629fad338f1fde3
trinikaka <73846745+trinikaka@users.noreply.github.com>
2024-04-04 21:21:50 -0400

    Entry page, Index Page and Search Specifications complete

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -20,8 +20,30 @@ def index(request):
 def entry(request, title):
     html_content= convert_md_to_html(title)
     if html_content == None:
-        return render(request, "encyclopedia/error.html")
+        return render(request, "encyclopedia/error.html", {
+            "message": "This entry does not exist"
+        })
     else:
-        return render (request, "encyclopedia/entry.html" )
+        return render (request, "encyclopedia/entry.html",{
+            "title" : title,
+            "content" : html_content
+        } )
 
-
+def search (request):
+    if request.method == "POST":
+        entry_search = request.POST ['s']
+        html_content = convert_md_to_html (entry_search) 
+        if html_content is not None:
+            return render (request, "encyclopedia/entry.html",{
+                "title" : entry_search,
+                "content" : html_content
+            })
+        else:
+            allEntries = util.list_entries()
+            suggestion = []
+            for entry in allEntries:
+                if entry_search.lower() in entry.lower():
+                    suggestion.append(entry)
+            return render(request, "encyclopedia/search.html", {
+                "suggestion" : suggestion
+            })    
